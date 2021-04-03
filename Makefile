264312463d67c8b8ced848905ccc9bbe72234355
BSO <ab7580@student.uni-lj.si>
2021-04-03 11:07:29 +0200

    messing around

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-PROGRAM=blink
+PROGRAM=pressure
 EXTRA_COMPONENTS=extras/i2c extras/bmp280
 include ../../common.mk
--- pressure.c
+++ pressure.c
@@ -1,78 +1,80 @@
+
 #include <stdlib.h>
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "esp8266.h"
-
-
-void showTasks(void *pvParameters)
-{
-    while(1) {
-    	static char msg[500];
-    	vTaskList(msg);
-    	puts(msg);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-
-    vTaskDelete(NULL);
-}
-
-void CountToThree(void) {
-	while(1) {
-	    	puts("1");
-	        vTaskDelay(1000 / portTICK_PERIOD_MS);
-	        puts("2");
-	        vTaskDelay(1000 / portTICK_PERIOD_MS);
-	        puts("3");
-	        vTaskDelay(1000 / portTICK_PERIOD_MS);
-	    }
-
-	    vTaskDelete(NULL);
-}
-
-void alenTask(void) {
-	while (1) {
-		puts("alen");
-		vTaskDelay(5000 / portTICK_PERIOD_MS);
-	}
-	vTaskDelete(NULL);
-}
+#include "i2c/i2c.h"
+#include "bmp280/bmp280.h"
 
 const int gpio = 2;
 
-/* This task uses the high level GPIO API (esp_gpio.h) to blink an LED.
- *
- */
-void blinkenTask(void *pvParameters)
-{
-    gpio_enable(gpio, GPIO_OUTPUT);
-    while(1) {
-        gpio_write(gpio, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        gpio_write(gpio, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+#define I2C_BUS		0
+#define SCL_PIN		14
+#define SDA_PIN		12
+#define PCF_ADDRESS 0x38
+
+#define button1		0x20	// 0b ??0? ????
+#define button2		0x10	// 0b ???0 ????
+#define button3		0x80	// 0b 0??? ????
+#define button4		0x40	// 0b ?0?? ????
+
+#define led1 		0xfe	// 0b ???? ???0
+#define led2 		0xfd	// 0b ???? ??0?
+#define led3 		0xfb	// 0b ???? ?0??
+#define led4 		0xf7	// 0b ???? 0???
+#define leds_off	0xff
+
+bmp280_t bmp280_dev;
+
+// read BMP280 sensor values
+float read_bmp280_forced() {
+
+	float temperature, pressure;
+
+	bmp280_force_measurement(&bmp280_dev);
+	// wait for measurement to complete
+	while (bmp280_is_measuring(&bmp280_dev)) {
+	};
+	bmp280_read_float(&bmp280_dev, &temperature, &pressure, NULL);
+
+	return pressure;
 }
+
+void write_byte_pcf(uint8_t data) {
+	i2c_slave_write(I2C_BUS, PCF_ADDRESS, NULL, &data, 1);
+}
+
+uint8_t read_byte_pcf() {
+	uint8_t data;
+
+	i2c_slave_read(I2C_BUS, PCF_ADDRESS, NULL, &data, 1);
+	return data;
+}
+
+void ReadPeriodically(void) {
+	while (1) {
+
+	}
+}
+
+
 
 void user_init(void)
 {
     uart_set_baud(0, 115200);
-    xTaskCreate(CountToThree, "CountToThree", 256, NULL, 2, NULL);
-    xTaskCreate(alenTask, "alenTask", 256, NULL, 2, NULL);
-    xTaskCreate(blinkenTask, "blink", 256, NULL, 2, NULL);
-    // xTaskCreate(pxTaskCode, pcName, usStackDpeth, pvParameter, uxPriority, pxCreatedTask)
-    /*
-     * xTaskCreate(params).
-     * Vrne, ali je uspeˇsno ali ne.
-     * Params:
-     * –pvTaskCode:  katera funkcija bo izvajala opravilo–pcName:  ime opravila
-     * –uStackDepth:   globina  sklada  (koliko  elementov  lahko  uporablja  to  opravilo).   Dobra  privzetavrednost je 1000
-     * –pvParameters:  parametri, ki jih lahko poˇsljemo v funkcijo pvTaskCode()
-     * –uxPriority:  prioriteta
-     * –pxCreatedTask:  kazalec na opravilo
-     */
+    i2c_init(I2C_BUS, SCL_PIN, SDA_PIN, I2C_FREQ_100K);
+	gpio_enable(SCL_PIN, GPIO_OUTPUT);
 
-    // this is a comment
-	// this is another one
+    // BMP280 configuration
+	bmp280_params_t params;
+	bmp280_init_default_params(&params);
+	params.mode = BMP280_MODE_FORCED;
+	bmp280_dev.i2c_dev.bus = I2C_BUS;
+	bmp280_dev.i2c_dev.addr = BMP280_I2C_ADDRESS_0;
+	bmp280_init(&bmp280_dev, &params);
+
+
+    xTaskCreate(ReadPeriodically, "ReadPeriodically", 256, NULL, 2, NULL);
 }
