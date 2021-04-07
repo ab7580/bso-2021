ce574ba45be46d2ca72f6f469d001878526b0250
BSO <ab7580@student.uni-lj.si>
2021-04-07 15:59:34 +0200

    simple working semafor example

--- blink.c
+++ blink.c
@@ -6,6 +6,7 @@
 #include "esp8266.h"
 #include "i2c/i2c.h"
 #include "bmp280/bmp280.h"
+#include "semphr.h"
 
 const int gpio = 2;
 
@@ -157,6 +158,65 @@ void komunikacijaTask(void *pvParameters)
 	}
 }
 
+SemaphoreHandle_t xSemaphoreA = NULL;
+SemaphoreHandle_t xSemaphoreB = NULL;
+
+ void TaskA( void * pvParameters )
+ {
+	while(1) {
+	    if( xSemaphoreA != NULL )
+	    {
+	        if( xSemaphoreTake( xSemaphoreA, 0 ) == pdTRUE )
+	        {
+	        	xSemaphoreGive(xSemaphoreA);
+	            puts("taskA");
+	        }
+	    }
+	    vTaskDelay(2000 / portTICK_PERIOD_MS);
+	}
+	vTaskDelete(NULL);
+ }
+ void TaskB( void * pvParameters )
+ {
+	while(1) {
+	    if( xSemaphoreB != NULL )
+	    {
+	        if( xSemaphoreTake( xSemaphoreB, 0 ) == pdTRUE )
+	        {
+	        	xSemaphoreGive(xSemaphoreB);
+	            puts("taskB");
+	        }
+	    }
+	    vTaskDelay(2000 / portTICK_PERIOD_MS);
+	}
+	vTaskDelete(NULL);
+ }
+ void MasterTask( void * pvParameters )
+ {
+	int8_t data;
+	xSemaphoreA = xSemaphoreCreateBinary();
+	xSemaphoreB = xSemaphoreCreateBinary();
+	xSemaphoreTake(xSemaphoreA, 0);
+	xSemaphoreTake(xSemaphoreB, 0);
+	xTaskCreate(TaskA, "TaskA", 256, NULL, 2, NULL);
+	xTaskCreate(TaskB, "TaskB", 256, NULL, 2, NULL);
+	while(1) {
+		data = read_byte_pcf();
+		if((data & button1) == 0){
+			printf("\nBTN/LED1 - enable task B, disable task A");
+			xSemaphoreTake(xSemaphoreA, 10);
+			xSemaphoreGive(xSemaphoreB);
+		}
+		if ((data & button2) == 0) {
+			printf("\nBTN/LED2 - enable task A, disable task B");
+			xSemaphoreTake(xSemaphoreB, 10);
+			xSemaphoreGive(xSemaphoreA);
+		}
+		vTaskDelay(100 / portTICK_PERIOD_MS);
+	}
+	vTaskDelete(NULL);
+ }
+
 
 void user_init(void)
 {
@@ -178,8 +238,8 @@ void user_init(void)
 	bmp280_dev.i2c_dev.addr = BMP280_I2C_ADDRESS_0;
 	bmp280_init(&bmp280_dev, &params);
 
-    xTaskCreate(ReadPressureEverySecond, "ReadPressureEverySecond", 256, NULL, 2, NULL);
-    xTaskCreate(GetLastTenPressuresAverage, "GetLastTenPressuresAverage", 256, NULL, 3, NULL);
-	xTaskCreate(komunikacijaTask, "komunikacijaTask", 256, NULL, 2, NULL);
+    // xTaskCreate(ReadPressureEverySecond, "ReadPressureEverySecond", 256, NULL, 2, NULL);
+    // xTaskCreate(GetLastTenPressuresAverage, "GetLastTenPressuresAverage", 256, NULL, 3, NULL);
+	// xTaskCreate(komunikacijaTask, "komunikacijaTask", 256, NULL, 2, NULL);
+	xTaskCreate(MasterTask, "MasterTask", 256, NULL, 3, NULL);
 }
-
